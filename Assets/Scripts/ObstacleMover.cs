5dfc7458fb2cd43fea8a0a24d0d8a7d4a9a17348
Atilla Akkaya <118857762+dutpekmezi@users.noreply.github.com>
2026-01-28 19:45:48 +0300

    Add paired obstacle gaps

--- Assets/Scripts/ObstacleMover.cs
+++ Assets/Scripts/ObstacleMover.cs
@@ -9,6 +9,59 @@ public class ObstacleMover : MonoBehaviour
     [SerializeField] private GameObject body;
     [SerializeField] private GameObject bottom;
 
+    private Vector3 initialTopLocalPosition;
+    private Vector3 initialBodyLocalScale;
+    private Vector3 initialBottomLocalPosition;
+
+    private void Awake()
+    {
+        if (top != null)
+        {
+            initialTopLocalPosition = top.transform.localPosition;
+        }
+
+        if (body != null)
+        {
+            initialBodyLocalScale = body.transform.localScale;
+        }
+
+        if (bottom != null)
+        {
+            initialBottomLocalPosition = bottom.transform.localPosition;
+        }
+    }
+
+    public void Configure(float bodyScaleY, bool flipVertically)
+    {
+        if (body != null)
+        {
+            Vector3 bodyScale = body.transform.localScale;
+            bodyScale.y = bodyScaleY;
+            body.transform.localScale = bodyScale;
+
+            float growthAmount = bodyScaleY - initialBodyLocalScale.y;
+
+            if (top != null)
+            {
+                top.transform.localPosition = initialTopLocalPosition + Vector3.up * growthAmount;
+            }
+
+            if (bottom != null)
+            {
+                bottom.transform.localPosition = initialBottomLocalPosition + Vector3.down * growthAmount;
+            }
+        }
+
+        if (flipVertically)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+        }
+        else
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+
     private void Update()
     {
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
