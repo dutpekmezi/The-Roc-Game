64e454211b6a80cc941277844d6a559b15f66f7a
Dut Pekmezi <sirakkaya@gmail.com>
2026-01-28 19:42:35 +0300

    update

--- Assets/Scripts/ObstacleMover.cs
+++ Assets/Scripts/ObstacleMover.cs
@@ -5,6 +5,10 @@ public class ObstacleMover : MonoBehaviour
     [SerializeField] private float moveSpeed = 2f;
     [SerializeField] private float destroyX = -12f;
 
+    [SerializeField] private GameObject top;
+    [SerializeField] private GameObject body;
+    [SerializeField] private GameObject bottom;
+
     private void Update()
     {
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
