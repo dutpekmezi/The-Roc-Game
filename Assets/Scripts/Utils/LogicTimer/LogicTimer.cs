367f897fe314abb73dee840860899218a29038e3
Dut Pekmezi <sirakkaya@gmail.com>
2026-01-30 23:44:11 +0300

    update

--- Assets/Scripts/Utils/LogicTimer/LogicTimer.cs
+++ Assets/Scripts/Utils/LogicTimer/LogicTimer.cs
@@ -11,7 +11,7 @@ namespace Utils.LogicTimer
 
     public class LogicTimer : IDisposable
     {
-        public const float FramesPerSecond = 30f;
+        public const float FramesPerSecond = 30;
         public const float FixedDelta = 1f / FramesPerSecond;
 
         private double _totalTime;
