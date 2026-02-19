4016f288e45d5c6d82d7b8d10194519d25e7d1a1
Atilla Akkaya <118857762+dutpekmezi@users.noreply.github.com>
2026-02-19 21:41:32 +0300

    Add configurable spin reward system and bind SpinPopUp frames

---
+++ Assets/Scripts/Game/Systems/Spin/SpinRewardSettings.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Systems
+{
+    [CreateAssetMenu(fileName = "SpinRewardSettings", menuName = "Game/Spin/Spin Reward Settings")]
+    public class SpinRewardSettings : ScriptableObject
+    {
+        public List<SpinRewardConfig> Rewards = new();
+    }
+}
