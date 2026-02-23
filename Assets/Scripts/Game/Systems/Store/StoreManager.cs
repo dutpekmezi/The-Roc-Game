16a4ddf809fa6ca4dc46a5ab8ddfdd17075e5348
Atilla Akkaya <118857762+dutpekmezi@users.noreply.github.com>
2026-02-23 13:21:38 +0300

    Add persistent purchased products save to StoreManager

--- Assets/Scripts/Game/Systems/Store/StoreManager.cs
+++ Assets/Scripts/Game/Systems/Store/StoreManager.cs
@@ -1,8 +1,35 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
+using Utils.Save;
 
 namespace Game.Systems
 {
+    public class PurchasedProductsEntity : ISaveable
+    {
+        public List<string> purchasedProductIds;
+
+        public PurchasedProductsEntity()
+        {
+            purchasedProductIds = new List<string>();
+        }
+
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public T Deserialize<T>(string data) where T : ISaveable, new()
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                return JsonConvert.DeserializeObject<T>(null);
+            }
+
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+    }
+
     public class StoreManager : BaseSystem
     {
         private StoreSettings storeSettings;
@@ -10,6 +37,7 @@ namespace Game.Systems
 
         private List<string> purchasedProductIds = new List<string>();
         public List<string> PurchasedProductIds => purchasedProductIds;
+        private SaveRepository<PurchasedProductsEntity> purchasedProductsRepo;
         public static StoreManager Instance { get; private set; }
 
         public StoreManager(StoreSettings storeSettings) 
@@ -22,6 +50,11 @@ namespace Game.Systems
             Instance = this;
 
             this.storeSettings = storeSettings;
+
+            SaveService.Instance.Register<PurchasedProductsEntity>("store_purchased_products");
+            purchasedProductsRepo = SaveService.Instance.GetRepository<PurchasedProductsEntity>();
+            purchasedProductsRepo.Load();
+            purchasedProductIds = purchasedProductsRepo.Get().purchasedProductIds;
         }
 
         public string RegisterPurchasedProduct(string productId)
@@ -29,6 +62,7 @@ namespace Game.Systems
             if (!purchasedProductIds.Contains(productId))
             {
                 purchasedProductIds.Add(productId);
+                purchasedProductsRepo.Save(purchasedProductsRepo.Get());
             }
 
             return productId;
