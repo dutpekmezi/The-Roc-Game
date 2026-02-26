5c579cc366aa9ac7928ff0998937ca297d671e40
Dut Pekmezi <sirakkaya@gmail.com>
2026-02-26 15:52:04 +0300

    update

---
+++ Assets/Scripts/FireBase/FirebaseSmokeTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Firebase;
+using Firebase.Extensions;
+using Firebase.Firestore;
+
+public class FirebaseSmokeTest : MonoBehaviour
+{
+    string docId = "jyOw9zXFQhAk50na6KMT";
+
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.Result != DependencyStatus.Available)
+            {
+                Debug.LogError("Firebase init failed: " + task.Result);
+                return;
+            }
+
+            Debug.Log("✅ Firebase initialized");
+
+            var db = FirebaseFirestore.DefaultInstance;
+
+            db.Collection("QRCodes").Document(docId)
+                .GetSnapshotAsync()
+                .ContinueWithOnMainThread(t =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        Debug.LogError("Firestore read error");
+                        return;
+                    }
+
+                    var snap = t.Result;
+
+                    if (snap.Exists)
+                        Debug.Log("✅ Firestore bağlantısı OK");
+                    else
+                        Debug.Log("❌ Document bulunamadı");
+                });
+        });
+    }
+}
